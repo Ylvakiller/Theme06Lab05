f1926ecf12b26845836e43731a5e2000eb796e63
Ylvakiller <rem.geuze@gmail.com>
2015-01-08 21:41:44 +0100

    Added first descriptions to the main java files

---
+++ ReaderAndWriter/readerWriter/ReaderAndWriter.java
@@ -0,0 +1,22 @@
+package readerWriter;
+/*
+ * Description as found on blackboard:
+ * 2. Write a program that reads a file of numbers of type int and writes all the numbers to another file, 
+ * but without any duplicate numbers. 
+ * Assume that the numbers in the input file are already ordered from smallest to largest. 
+ * After the program is run, the new file will contain all the numbers in the original file, 
+ * but no number will appear more than once in the file. 
+ * The numbers in the output file should also be sorted from smallest to largest. 
+ * Your program should obtain both file names from the user. 
+ * Use either a text file or a binary file. 
+ * For the text-file version, assume one number per line. 
+ * For the binary-file version, use numbers of type int that are written using writeInt.
+ */
+public class ReaderAndWriter {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
