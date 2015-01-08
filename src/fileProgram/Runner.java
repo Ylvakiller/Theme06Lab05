3f4069857a6939a6d4981b0e31a391377c53d651
Ylvakiller <rem.geuze@gmail.com>
2015-01-08 22:05:14 +0100

    Moved to a single class system for this project

---
+++ src/fileProgram/Runner.java
@@ -0,0 +1,19 @@
+package fileProgram;
+
+/**
+ * This class will be able to do all the tasks as discussed in the program, that is reading int values from a file, 
+ * calculating the lowest and highest number, calculating the average, 
+ * and creating a new file with all the numbers only occurring ones and in order from lowest to highest number
+ */
+
+/*
+ * Descriptions as found on blackboard are saved in the description file in this project folder
+ */
+public class Runner {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
