3d75f622226b85fd413a126230adbb674ad6811a
Ylvakiller <rem.geuze@gmail.com>
2015-01-10 17:07:19 +0100

    theoretically made the addIntToArray method
    UNTESTED!

--- src/fileProgram/Numbers.java
+++ src/fileProgram/Numbers.java
@@ -19,7 +19,7 @@ public class Numbers {
 	/**
 	 * An array with all the numbers sorted from low to high, with no number occurring twice
 	 */
-	private static int[] sortedint;
+	private static int[] sortedInt;
 	/**
 	 * The total amount of numbers that this class has come across
 	 */
@@ -49,6 +49,19 @@ public class Numbers {
 	 * @param toAdd the number to add to the array
 	 */
 	private static void addIntToArray(int toAdd){
+		int i = 0;
+		while(sortedInt[i]<toAdd&&i<sortedInt.length){	//this will go to the place in the array where the number to add has to be
+			i++;
+		}
+		if (sortedInt[i]!=toAdd){//this will check if the number is already in the array
+			while (i<sortedInt.length){	//this should move all the variables up one place in the array until the array is done
+				int temp = sortedInt[i];
+				sortedInt[i] = toAdd;
+				toAdd = temp;
+				i++;
+			}
+			sortedInt[i] = toAdd;	//add the last number to the array, as such increasing the size by one
+		}
 		
 	}
 }
