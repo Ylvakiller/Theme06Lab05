2bee7e466fdefca8740cf39d46c2233084938343
Ylvakiller <rem.geuze@gmail.com>
2015-01-10 16:51:15 +0100

    Started the numbers class

---
+++ src/fileProgram/Numbers.java
@@ -0,0 +1,35 @@
+package fileProgram;
+
+import java.math.BigInteger;
+
+/**
+ * This class is meant to do all the calculations for different parts of this file program
+ * @author Ylva
+ *
+ */
+public class Numbers {
+	/**
+	 * The smallest number that this instance of this class has come across
+	 */
+	private int smallestNumber;
+	/**
+	 * The largest number that this instance of this class has come across
+	 */
+	private int largestNumber;
+	/**
+	 * An array with all the numbers sorted from low to high, with no number occurring twice
+	 */
+	private int[] sortedint;
+	/**
+	 * This is the instance of the FileActions class that is needed for this program
+	 */
+	private FileActions file;
+	/**
+	 * The total amount of numbers that this class has come across
+	 */
+	private int amountOfNumbers;
+	/**
+	 * the sum of all the numbers that this class has come across
+	 */
+	private BigInteger total;
+}
